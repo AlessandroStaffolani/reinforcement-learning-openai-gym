b4227c2b1d2c0720a08d33b9f4a7097a36e21072
Alessandro Staffolani <alestam93@gmail.com>
2019-10-01 10:37:29 +0100

    Working on Atari

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import gym
 from algorithms.q_learning import QLearning
 from utils.gym_wrapper import wrap_env
 from algorithms.discretization_functions import discretize_cartpole_v0
+from preprocessing.atari import imshow, preprocess_breakout
 
 
 def main():
@@ -26,6 +27,27 @@ def main():
     print(stats)
 
     env.close()
+
+
+# def test():
+#     env_name = "Breakout-v0"
+#
+#     env = gym.make(env_name)
+#     frame = env.reset()
+#     imshow(frame)
+#     imshow(preprocess_breakout(frame))
+#     for i_episode in range(1):
+#         frame = env.reset()
+#         for t in range(10):
+#             env.render()
+#             imshow(preprocess_breakout(frame))
+#             action = env.action_space.sample()
+#             frame, reward, done, info = env.step(action)
+#             if done:
+#                 print("Episode finished after {} timesteps".format(t + 1))
+#                 break
+#     env.close()
+
 
 if __name__ == "__main__":
     main()
